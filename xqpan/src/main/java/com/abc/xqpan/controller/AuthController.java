4a5178e4e3ac38979d5a91858932c149f5bcd73e
lijunxi <2770063826@qq.com>
2023-05-29 17:33:28 +0800

    hello

--- xqpan/src/main/java/com/abc/xqpan/controller/AuthController.java
+++ xqpan/src/main/java/com/abc/xqpan/controller/AuthController.java
@@ -1,9 +1,20 @@
 package com.abc.xqpan.controller;
 
+import com.abc.xqpan.entity.Result;
+import com.abc.xqpan.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+
+
+    @PostMapping("/login")
+    public Result<String> login(@RequestBody User user){
+        return Result.success("ok");
+    }
 }
